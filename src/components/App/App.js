b76c0971057f4f53cffba6296384e3355051350f
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-27 23:15:54 -0400

    Add sweet alert

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,48 @@ import './App.css';
 import {AddForm} from "../AddForm/AddForm";
 import {TaskTable} from "../TaskTable/TaskTable";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SweetAlert2 from 'react-sweetalert2';
 
 function App() {
 
     const [taskList, setTaskList] = useState(JSON.parse(sessionStorage.getItem("taskList")) ? JSON.parse(sessionStorage.getItem("taskList")) : []);
     const [taskName, setTaskName] = useState('');
     const [taskDeadline, setTaskDeadline] = useState('')
+    const [swalProps, setSwalProps] = useState({});
 
     useEffect(() => {
         sessionStorage.setItem("taskList", JSON.stringify(taskList))
     }, [taskList])
+
+    const showSuccessAlert = () => {
+        setSwalProps({
+            show: true,
+            title: `Successfully added new task`,
+            icon: 'success',
+            showConfirmButton: false,
+        });
+        setTimeout(() => setSwalProps({
+            show: false,
+            title: `Successfully added new task`,
+            icon: 'success',
+            showConfirmButton: false,
+        }), 1500);
+    }
+
+    const showDeleteAlert = () => {
+        setSwalProps({
+            show: true,
+            title: `Successfully deleted`,
+            icon: 'success',
+            showConfirmButton: false,
+        });
+        setTimeout(() => setSwalProps({
+            show: false,
+            title: `Successfully deleted`,
+            icon: 'success',
+            showConfirmButton: false,
+        }), 1500);
+    }
 
     const addNewTask = (taskName, taskDeadline) => {
         setTaskList([
@@ -25,17 +57,17 @@ function App() {
     }
 
     const handleSubmit = (e) => {
-        console.log("submit")
         e.preventDefault();
         addNewTask(taskName, taskDeadline);
+        showSuccessAlert();
         setTaskName('');
         setTaskDeadline('');
     }
 
     const handleDelete = (i) => {
-        console.log(i);
         let newTaskList = [...taskList];
         newTaskList.splice(i, 1);
+        showDeleteAlert();
         setTaskList(newTaskList);
     }
 
@@ -48,6 +80,7 @@ function App() {
             <div className="taskTable">
                 <TaskTable taskList={taskList} setTaskList={setTaskList} handleDelete={handleDelete} />
             </div>
+            <SweetAlert2 title={swalProps.title} show={swalProps.show} icon={swalProps.icon} showConfirmButton={swalProps.showConfirmButton} />
         </div>
     );
 }
