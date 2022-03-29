b3fee0a24419f813513a2c6d00f41b67f0af9f09
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-29 03:50:06 -0400

    Get min date

--- src/components/Task/EditableTask.js
+++ src/components/Task/EditableTask.js
@@ -4,10 +4,10 @@ export const EditableTask = (props) => {
     return (
         <tr key={props.index}>
             <td>{props.index + 1}</td>
-            <td><input defaultValue={props.task.taskName} id="taskName" type="text" required onChange={(e) => {
+            <td><input style={{width: "100%"}}  defaultValue={props.task.taskName} id="taskName" type="text" required onChange={(e) => {
                 props.setName(e.target.value ? e.target.value : props.task.taskName);
             }}/></td>
-            <td><input defaultValue={props.task.taskDeadline} id="taskDeadline" type="date" required onChange={(e) => {
+            <td><input min={props.minDate} defaultValue={props.task.taskDeadline} id="taskDeadline" type="date" required onChange={(e) => {
                 props.setDeadline(e.target.value ? e.target.value : props.task.taskDeadline)
             }}/></td>
             <td style={{textAlign: "center"}}><Button variant="btn btn-outline-success" onClick={(e) => {
