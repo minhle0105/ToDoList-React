716c750026329baee4cec0829977c8e2dd0b5520
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-29 02:23:28 -0400

    button prevent default

--- src/components/Task/EditableTask.js
+++ src/components/Task/EditableTask.js
@@ -6,8 +6,8 @@ export const EditableTask = (props) => {
             <td>{props.index + 1}</td>
             <td><input id="taskName" type="text" required/></td>
             <td><input id="taskDeadline" type="date" required/></td>
-            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-danger" onClick={() => {props.showPopup(props.index, props.task.taskName)}}>Delete</Button></td>
-            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-success" onClick={() => {props.handleUpdateClick(props.index)}}>Update</Button></td>
+            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-danger" onClick={(e) => {e.preventDefault(); props.showPopup(props.index, props.task.taskName)}}>Delete</Button></td>
+            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-success" onClick={(e) => {e.preventDefault(); props.handleUpdateClick(props.index)}}>Update</Button></td>
         </tr>
     )
 }
--- src/components/Task/ReadOnlyTask.js
+++ src/components/Task/ReadOnlyTask.js
@@ -6,8 +6,8 @@ export const ReadOnlyTask = (props) => {
             <td>{props.index + 1}</td>
             <td>{props.task.taskName}</td>
             <td>{props.task.taskDeadline}</td>
-            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-danger" onClick={() => {props.showPopup(props.index, props.task.taskName)}}>Delete</Button></td>
-            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-success" onClick={() => {props.handleUpdateClick(props.index)}}>Update</Button></td>
+            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-danger" onClick={(e) => {e.preventDefault(); props.showPopup(props.index, props.task.taskName)}}>Delete</Button></td>
+            <td><Button style={{marginLeft: "20%"}} variant="btn btn-outline-success" onClick={(e) => {e.preventDefault(); props.handleUpdateClick(props.index)}}>Update</Button></td>
         </tr>
     )
 }
